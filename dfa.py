5271d2c3de8c16f1f8b1330fb526abbc42b66408
dokato <raymon92@gmail.com>
2015-10-13 22:55:12 +0200

    first working version

--- dfa.py
+++ dfa.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 # detrended fluctuation analysis
 
 def calc_rms(x, scale):
-    print x.shape[0], scale
     shape = (x.shape[0]//scale, scale)
+    print shape 
     X = np.lib.stride_tricks.as_strided(x,shape=shape)
     scale_ax = np.arange(scale)
     rms = np.zeros(X.shape[0])
@@ -15,20 +15,24 @@ def calc_rms(x, scale):
         rms[e] = np.sqrt(np.mean((xcut-xfit)**2))
     return rms
 
-def dfa(x, scale_lim=[3,10], scale_dens=0.25, show=False):
+def dfa(x, scale_lim=[5,9], scale_dens=0.25, show=False):
     y = np.cumsum(x - np.mean(x))
     scales = (2**np.arange(scale_lim[0], scale_lim[1], scale_dens)).astype(np.int)
     fluct = np.zeros(len(scales))
     for e, sc in enumerate(scales):
         fluct[e] = np.mean(np.sqrt(calc_rms(y, sc)**2))
-    coeff = np.polyfit(np.log2(scales), np.log2(fluct))
+    coeff = np.polyfit(np.log2(scales), np.log2(fluct), 1)
     if show:
-        plt.plot(scales, fluct, 'r', label='$\alpha$ = %0.2f'%coeff[0])
+        fluctfit = 2**np.polyval(coeff,np.log2(scales))
+        plt.loglog(scales, fluct, 'bo')
+        plt.loglog(scales, fluctfit, 'r', label='$\alpha$ = %0.2f'%coeff[0])
         plt.title('DFA')
         plt.xlabel('$log_{10}$(time window)')
         plt.ylabel('$log_{10}$<F(t)>')
+        plt.show()
     return scales, fluct, coeff[0]
 
 
-x = np.random.randn(1000)
-dfa(x)
+if __name__=='__main__':
+    x = np.random.randn(1000)
+    print dfa(x, show=1)
