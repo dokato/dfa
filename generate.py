8170eaddc6787dd16a50b164e43f4c4ef2dc9f9f
dokato <raymon92@gmail.com>
2016-02-12 14:47:51 +0100

    generate power law data fixed

--- generate.py
+++ generate.py
@@ -2,11 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from dfa import dfa
 
-###
-# is alpha really DFA alpha ??
-##
 
-def power_law_noise(n, alpha, var):
+def power_law_noise(n, alpha, var=1):
     '''
     
     Based on:
@@ -16,13 +13,15 @@ def power_law_noise(n, alpha, var):
     # computing standard deviation from variance
     stdev = np.sqrt(np.abs(var))
     
+    beta = 2*alpha-1
+    
     hfa = np.zeros(2*n)
     hfa[0] = 1
     for i in range(1,n):
-        hfa[i] = hfa[i-1] * (0.5*alpha + (i-1))/(i)
+        hfa[i] = hfa[i-1] * (0.5*beta + (i-1))/i
     
     # sample white noise
-    wfa = np.hstack((stdev * np.random.randn(n), np.zeros(n)))
+    wfa = np.hstack((-stdev +2*stdev * np.random.rand(n), np.zeros(n)))
     
     fh = np.fft.fft(hfa)
     fw = np.fft.fft(wfa)
@@ -34,18 +33,15 @@ def power_law_noise(n, alpha, var):
     
     # matching the conventions of the Numerical Recipes
     
-    ftot[1]  /= 2
-    ftot[-1] /= 2
-
     ftot = np.hstack((ftot, np.zeros(n-1)))
     
     x = np.fft.ifft(ftot)
     
-    return 2.* np.real(x[:n])
+    return np.real(x[:n])
 
-x1 = power_law_noise(2048, 0.5, 3)
-x2 = power_law_noise(2048, 0.7, 3)
-x3 = power_law_noise(2048, 1.2, 3)
+x1 = power_law_noise(2**12, 0.5)
+x2 = power_law_noise(2**12, 0.8)
+x3 = power_law_noise(2**12, 1.2)
 
 plt.subplot(311)
 plt.plot(x1)
@@ -56,7 +52,6 @@ plt.plot(x3)
 
 plt.show()
 
-scales, fluct, alpha = dfa(x3, show=1)
-print scales
-print fluct
-print "DFA exponent: {}".format(alpha)
+for e,xx in enumerate([x1,x2,x3]): 
+    scales, fluct, alpha = dfa(xx)
+    print "DFA exponent {}: {}".format(e+1, alpha)
