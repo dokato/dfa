b21c1388c650871e539362dd6833e16d26b06e8d
dokato <raymon92@gmail.com>
2016-02-15 16:24:16 +0100

    updated readme and comments

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ It bases on these two articles:
 * Hardstone, R. et al. Detrended fluctuation analysis: A scale-free view on neuronal oscillations. Front. Physiol. 3 NOV, 1–13 (2012).
 * Ihlen, E. a F. Introduction to multifractal detrended fluctuation analysis in Matlab. Front. Physiol. 3 JUN, 1–18 (2012).
 
+You may test it using power law data generator from `generator.py`.
+
 Requirements
 ------------
 
--- dfa.py
+++ dfa.py
@@ -19,13 +19,16 @@ def calc_rms(x, scale):
       *rms* : numpy.array
         RMS data in each window with length len(x)//scale
     """
+    # making an array with data divided in windows
     shape = (x.shape[0]//scale, scale)
     X = np.lib.stride_tricks.as_strided(x,shape=shape)
+    # vector of x-axis points to regression
     scale_ax = np.arange(scale)
     rms = np.zeros(X.shape[0])
     for e, xcut in enumerate(X):
         coeff = np.polyfit(scale_ax, xcut, 1)
         xfit = np.polyval(coeff, scale_ax)
+        # detrending and computing RMS of each window
         rms[e] = np.sqrt(np.mean((xcut-xfit)**2))
     return rms
 
@@ -58,11 +61,14 @@ def dfa(x, scale_lim=[5,9], scale_dens=0.25, show=False):
       *alpha* : float
         DFA exponent
     """
+    # cumulative sum of data with substracted offset
     y = np.cumsum(x - np.mean(x))
     scales = (2**np.arange(scale_lim[0], scale_lim[1], scale_dens)).astype(np.int)
     fluct = np.zeros(len(scales))
+    # computing RMS for each window
     for e, sc in enumerate(scales):
         fluct[e] = np.mean(np.sqrt(calc_rms(y, sc)**2))
+    # fitting a line to rms data
     coeff = np.polyfit(np.log2(scales), np.log2(fluct), 1)
     if show:
         fluctfit = 2**np.polyval(coeff,np.log2(scales))
@@ -79,9 +85,9 @@ def dfa(x, scale_lim=[5,9], scale_dens=0.25, show=False):
 if __name__=='__main__':
     n = 1000
     x = np.random.randn(n)
+    # computing DFA of signal envelope
     x = np.abs(ss.hilbert(x))
     scales, fluct, alpha = dfa(x, show=1)
     print scales
     print fluct
     print "DFA exponent: {}".format(alpha)
-
